e11f488e8c70db62f5dbb7d19310ac7d326d5191
Brandt Carlson <brandt.carlson11@gmail.com>
2023-06-20 13:41:42 -0500

    tweaks

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 class PokeName(FlaskForm):
     pokemon = StringField('Enter Pokemon', validators=[DataRequired()])
